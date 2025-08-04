e10448da8f7291627bff2758c1cf52135c800a9e
Thineth Gamage <thinethyt@gmail.com>
2025-08-04 11:35:16 +0300

    Added Lidar to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from multiprocessing import Process, Manager
 from webcam.blob_tracker import run_tracking
 from motors.controller import run_motor_control  # if used
+from LiDAR.lidar_handler import run_lidar
 from GUI import run_gui
 import numpy as np
 import time
@@ -22,9 +23,11 @@ if __name__ == "__main__":
         #p1 = Process(target=run_tracking, args=(shared_data,))
         #p2 = Process(target=run_motor_control, args=(shared_data,))
         p3 = Process(target=run_gui, args=(shared_data,))
+        p4 = Process(target=run_lidar, args=(shared_data,))
         #p1.start()
         #p2.start()
         p3.start()
+        p4.start()
 
         try:
             while not shared_data["shutdown"]:
@@ -36,7 +39,9 @@ if __name__ == "__main__":
         #p1.terminate()
         #p2.terminate()
         p3.terminate()
+        p4.terminate()
         #p1.join()
         #p2.join()
         p3.join
+        p4.join()
         print("Program exited cleanly")
