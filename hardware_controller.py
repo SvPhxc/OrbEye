9ca3c4ee8dd8aaca8f99d9c61bc6d579ea0a7a03
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-15 12:36:53 +0300

    Update hardware_controller.py

--- hardware_controller.py
+++ hardware_controller.py
@@ -29,7 +29,7 @@ SCAN_STEP_DEG = 1.0
 MAX_PAN_SPEED_DPS = 1000.0
 PAN_KP, PAN_KI, PAN_KD = 20.0, 0.00, 0.00  # Gains can be more aggressive with this new logic
 MAX_TILT_SPEED_DPS = 600.0
-TILT_KP, TILT_KI, TILT_KD = 20.0, 0.00, 0.00
+TILT_KP, TILT_KI, TILT_KD = 8.0, 0.01, 0.05
 
 
 # ==============================================================================
