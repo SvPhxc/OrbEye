7c747d0b6aee1a930f242a42a82b93583fd7a9cc
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-17 12:02:19 +0300

    Update hardware_controller.py

--- hardware_controller.py
+++ hardware_controller.py
@@ -59,9 +59,9 @@ SCAN_PAN_CALIBRATION_OFFSET_DEG = 0.5  # <--- TUNE THIS VALUE
 # 4. If needed, add a very small PAN_KI to help the motor hold its final position accurately.
 #
 MAX_PAN_SPEED_DPS = 800.0
-PAN_KP, PAN_KI, PAN_KD = 10.5, 0.0001, 0.03
+PAN_KP, PAN_KI, PAN_KD = 10, 0.0001, 0.05
 MAX_TILT_SPEED_DPS = 600.0
-TILT_KP, TILT_KI, TILT_KD = 10.5, 0.000, 0.000
+TILT_KP, TILT_KI, TILT_KD = 10, 0.000, 0.000
 
 
 # ==============================================================================
