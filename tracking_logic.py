3f2db7c3038f2758f422e7fecf14b6afa1e18946
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-18 09:55:34 +0300

    Update tracking_logic.py

--- tracking_logic.py
+++ tracking_logic.py
@@ -596,7 +596,7 @@ def run_tracking_logic(shared_data):
                                 shared_data["ekf_initialized"].value = True
                                 shared_data["acquire_points"].value = False
                                 shared_data["acquirer_status"].value = 0
-                elif shared_data["lidar_track_mode_active"].value
+                elif shared_data["lidar_track_mode_active"].value:
                     if state != TrackingState.TRACKING:
                         print("[TrackingLogic] Switching to TRACKING mode (Predictive)")
                         state = TrackingState.TRACKING
