b4b985e024946479471c06d038cde1633065c204
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-18 11:05:31 +0300

    Update tracking_logic.py

--- tracking_logic.py
+++ tracking_logic.py
@@ -24,7 +24,7 @@ class ClutterFilter:
     significantly closer than the known background object.
     """
 
-    def __init__(self, background_file="background_data.npy", angular_tolerance=1, distance_margin_cm=70.0):
+    def __init__(self, background_file="background_data.npy", angular_tolerance=0.5, distance_margin_cm=120.0):
         """
         Initializes the filter with a 2D (azimuth, elevation) background map.
 
