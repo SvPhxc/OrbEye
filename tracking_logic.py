2022e8dad6d458ce0b7da7c2ca2a9b86d8839788
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-18 14:17:17 +0300

    Update tracking_logic.py

--- tracking_logic.py
+++ tracking_logic.py
@@ -354,7 +354,7 @@ class HandTracker:
         Dynamically adjusts scan points and speed based on the target's distance.
         This is the core of the distance-based adaptation.
         """
-        shared_data["satellite_points"].value.append((self.best_point['az'], self.best_point['el'], distance_m/ 100, self.best_point['strength'], self.best_point['time']))
+        shared_data["satellite_points"].value.append((self.best_point['az'], self.best_point['el'], distance_m* 100, self.best_point['strength'], self.best_point['time']))
         # 1. SCAN RADIUS: Set to half the LiDAR's FOV.
         # This ensures the edge of the LiDAR's sensing cone passes through the
         # last known target position, maximizing the chance of a hit in a tight circle.
