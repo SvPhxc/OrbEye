3656937996cbfa0bc05a8f3e620273cf6bc3c945
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-19 21:50:22 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -186,8 +186,8 @@ class TrackerWindow(QtWidgets.QMainWindow):
         lcd_layout.addWidget(self.lcd_strength, 3, 1)
         controls_layout.addLayout(lcd_layout)
 
-        self.chk_reactive_mode = QCheckBox("Enable Reactive Mode (No Prediction)")
-        self.chk_reactive_mode.toggled.connect(self.on_reactive_mode_toggled)
+        self.btn_reactive_mode = QCheckBox("Enable Reactive Mode (No Prediction)")
+        self.btn_reactive_mode.toggled.connect(self.on_reactive_mode_toggled)
         controls_layout.addWidget(self.chk_reactive_mode)
 
         controls_layout.addStretch()
