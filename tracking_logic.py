ae95f6dd5f541582d302baa9656fa4007668ef86
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-21 22:23:23 +0300

    Update tracking_logic.py

--- tracking_logic.py
+++ tracking_logic.py
@@ -624,7 +624,7 @@ def _start_orbit_patrol(shared_data):
                 continuous_spiral_search = True ,# <-- ADD THIS LINE
                 spiral_radius_deg = 5,
                 spiral_rotations = 0,
-                sspiral_settle_s = 0.002
+                spiral_settle_s = 0.002
                 # <-- NEW (optional; 2 rotations)
                  # <-- NEW (optional; True to keep searching after the patrol)
             # <-- NEW (optional; 720 gives 0.5° spacing)
