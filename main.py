c9c6628cddd1888f7555a90d9b484faa17a15741
rhylkiio <55242744+rhylkiio@users.noreply.github.com>
2025-08-21 16:42:15 +0300

    Grafana visualisation

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import traceback
 from tracking_logic import run_tracker_process
 from tle_generator import run_tle_generator
 from orbital_tracker import run_circular_tracker
-#from grafana_visualisation import publish_data_to_aws
+from grafana_visualisation import publish_data_to_aws
 
 # Import all process functions
 from hardware_controller import run_hardware_controller
@@ -185,7 +185,7 @@ if __name__ == "__main__":
         "GUI": Process(target=run_gui, args=(shared_data,)),
         "TrackingLogic": Process(target=run_tracker_process, args=(shared_data,)),
         "TLEGenerator": Process(target=run_tle_generator, args=(shared_data,)),  # <-- ADD THE NEW PROCESS
-        #"GrafanaVisualization": Process(target=publish_data_to_aws, args=(shared_data,)),
+        "GrafanaVisualization": Process(target=publish_data_to_aws, args=(shared_data,)),
         "TLEGenerator": Process(target=run_tle_generator, args=(shared_data,)),
         "tracker_process":  Process(target=run_circular_tracker, args=(shared_data,)),
     # <-- ADD THE NEW PROCESS
