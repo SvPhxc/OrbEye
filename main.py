8cb1861d20464431ae169b919fd78223a6688161
sparnel07 <90954252+sparnel07@users.noreply.github.com>
2025-08-21 03:02:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
         "GUI": Process(target=run_gui, args=(shared_data,)),
         "TrackingLogic": Process(target=run_tracker_process, args=(shared_data,)),
         "TLEGenerator": Process(target=run_tle_generator, args=(shared_data,)),  # <-- ADD THE NEW PROCESS
-        "GrafanaVisualization": Process(target=publish_data_to_aws, args=(shared_data,))
+        "GrafanaVisualization": Process(target=publish_data_to_aws, args=(shared_data,)),
         "TLEGenerator": Process(target=run_tle_generator, args=(shared_data,)),
         "tracker_process": = Process(target=run_circular_tracker, args=(shared_data,))# <-- ADD THE NEW PROCESS
     }
